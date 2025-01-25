f1d731f7a0ed2d3df878fcbb451429db0ad70af0
techno123f <154799042+techno123f@users.noreply.github.com>
2025-01-25 23:09:33 +0530

    Create appium-config.js

---
+++ appium-config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  platformName: "Android", 
+  platformVersion: "12.0", 
+  deviceName: "emulator-5554", 
+  appPackage: "com.reddit.frontpage", 
+  appActivity: "com.reddit.frontpage.MainActivity",
+  automationName: "UiAutomator2", 
+};
